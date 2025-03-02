7c20b27e20f895f0c109c58f67b12542af028050
Divya <divya.inapakurthi@gmail.com>
2025-03-02 00:13:36 -0500

    backend ecommerce

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT
 const path = require("path")
 
 app.use(cors({
-    origin : "http://localhost:5173"
+    origin : "https://fashion-ecommerce-frontend.vercel.app"
 }))
 
 
